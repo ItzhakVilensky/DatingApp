565c125a889684b8251f998fa4a48b1fe1477d47
ItzhakVilensky <itzhak.vil@gmail.com>
2022-12-02 13:25:08 +0200

    *Add password + salt

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")] // api/account/register
+        public async Task<ActionResult<AppUser>> Register(string userName, string password)
+        {
+            using var hmac = new HMACSHA512(); // passwords' salt
+
+            var user = new AppUser
+            {
+                UserName = userName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Add(user);
+
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
