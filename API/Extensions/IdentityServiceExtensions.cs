308ba6195a06cea9069cf7988c77199cd2ab65a0
ItzhakVilensky <itzhak.vil@gmail.com>
2022-12-03 17:29:53 +0200

    * Add extensions methos for IServiceCollection

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,27 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services,
+    IConfiguration config)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding
+                .UTF8.GetBytes(config["TokenKey"])),
+                ValidateIssuer = false, // issuer is WebApi server no need to validate it
+                ValidateAudience = false // i am not using audience in JWT structure
+            };
+        });
+
+        return services;
+    }
+}
