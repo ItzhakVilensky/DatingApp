de86fd9aa1190baa20d661b5fb2f2fbaeed497de
ItzhakVilensky <itzhak.vil@gmail.com>
2022-12-05 21:38:00 +0200

    login + logout via account service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -7,12 +9,30 @@ import { Injectable } from '@angular/core';
 export class AccountService {
 
   baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) {
-
   }
 
-  login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model);
+  login(model: any): Observable<void> {
+    return this.http.post<User>(this.baseUrl + 'account/login', model)
+      .pipe(
+        map((response: User) => {
+          if (response) {
+            localStorage.setItem('user', JSON.stringify(response));
+            this.currentUserSource.next(response);
+          }
+        })
+      );
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
