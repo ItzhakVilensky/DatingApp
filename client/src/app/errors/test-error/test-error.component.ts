ea1eb4bc32b0893a5048e3cf69760bcbad8e7dd0
ItzhakVilensky <itzhak.vil@gmail.com>
2022-12-08 18:03:29 +0200

    * server error handled by interceptor on client

--- client/src/app/errors/test-error/test-error.component.ts
+++ client/src/app/errors/test-error/test-error.component.ts
@@ -9,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class TestErrorComponent implements OnInit {
 
   baseUrl = 'https://localhost:5001/api/';
-
+  valiadtionErrors: string[] = [];
+   
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -46,7 +47,10 @@ export class TestErrorComponent implements OnInit {
   get400ValidationError() {
     this.http.post(this.baseUrl + 'account/register', {}).subscribe({
       next: response => console.log(response),
-      error: err => console.log(err)
+      error: err => {
+        console.log(err);
+        this.valiadtionErrors = err;
+      }
     });
   }
 
