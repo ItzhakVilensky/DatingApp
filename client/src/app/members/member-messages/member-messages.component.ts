257766fff3ab4f4e583d34f089cd9490708add2d
ItzhakVilensky <itzhak.vil@gmail.com>
2023-01-15 22:36:27 +0200

    delete message functionality

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { IMessage } from 'src/app/_models/message';
+import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-messages',
@@ -10,4 +12,21 @@ export class MemberMessagesComponent {
 
   @Input() username?: string;
   @Input() messages?: IMessage[] | undefined = [];
+  messageContent = '';
+  @ViewChild('messageForm') messageForm?: NgForm;
+
+  constructor(private messageService: MessageService) {
+
+  }
+
+  sendMessage() {
+    if (!this.username) return;
+
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
+      next: message => {
+        this.messages?.push(message);
+        this.messageForm?.reset();
+      }
+    });
+  }
 }
